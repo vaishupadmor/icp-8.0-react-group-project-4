8c818ad5fe229422ec82938113c207fc05750375
vaishupadmor <vaishupadmor@gmail.com>
2024-09-10 23:24:24 +0530

    code-added

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -1,30 +1,42 @@
 import React from 'react'
 import "./footer.css"
-
+import Insta from "./insta-logo.jpg"
+import FB from "./facebook-logo.jpg"
+import Twitter from "./twitter.png"
+import YT from "./youtube.png"
 
 function Footer() {
   return (
     <div className='footer'>
         <div className='footer-main-container'>
+
+        <div className='footer-main'>
+         <h3 className='footer-head-para'> Esports</h3>
+         <p className='footer-para'> Esports is competitive level gaming. It's teams of 
+            people playing games against 
+            each other at a professional level, regularly winning 
+            huge sums of money as prizes </p>
+        </div>
         <div className='footer-main-1'>
          <h3 className='footer-head'> built for Gamers</h3>
 
-         <span ><a href='' className='footer-links'>Esports</a></span>
-         <span><a href='' className='footer-links'>Try playing </a></span>
-         <span><a href=''  className='footer-links'>tournaments</a> </span>
+         <span ><a href='https://esportsfederation.in/' className='footer-links'>Esports</a></span>
+         <span><a href='https://www.pcmag.com/picks/best-esports-games' className='footer-links'>Try playing </a></span>
+         <span><a href='https://www.esportsguide.com/events'  className='footer-links'>Tournaments</a> </span>
         
         </div>
         <div className='footer-main-1'>
-          <h3 className='footer-head'> About us</h3>
-          <span> <a> contact us </a></span>
-          <span> <a> follow us</a></span>
-          <span> <a> Get in touch</a></span>
+          <h3 className='footer-head'> Quick Links</h3>
+          <span> <a href='' className='footer-links'> contact us </a></span>
+          <span> <a href='' className='footer-links'> follow us</a></span>
+          <span> <a href='' className='footer-links'> About us</a></span>
         </div>
         <div className='footer-main-1'>
-          <h3 className='footer-head'> Social</h3>
-          <span> <a> instagram</a></span>
-          <span> <a> facebook</a></span>
-          <span> <a> twitter</a></span>
+          <h3 className='footer-head'> connect with us</h3>
+          <span><a href='' className='footer-links'> <img src={Insta} alt='instagram' style={{height:"32px" , borderRadius:"20px"}} /> </a></span>
+          <span> <a href='' className='footer-links'> <img src={FB} alt='facebook' style={{height:"32px" , borderRadius:"20px"}} /> </a></span>
+          <span> <a href='' className='footer-links'><img src={Twitter} alt='facebook' style={{height:"32px" , borderRadius:"20px"}} /> </a></span>
+          <span> <a href='' className='footer-links'><img src={YT} alt='facebook' style={{height:"32px" , borderRadius:"20px"}} /> </a></span>
         </div>
         </div>
         <hr></hr>
