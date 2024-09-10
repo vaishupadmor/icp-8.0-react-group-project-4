b063eeb9d87a68cc42774048bb1dc95931fac9c3
vaishupadmor <vaishupadmor@gmail.com>
2024-09-10 21:12:55 +0530

    card-changes

--- src/view/home/home.js
+++ src/view/home/home.js
@@ -1,23 +1,39 @@
 import React from 'react'
 import "./home.css"
 import Homecard from '../../component/homecards/homecard'
+import { ESPORTS_TEAM ,TAG_LINE,THEME,MAIN_TITLE,TAG_LINE2 } from '../../component/config/data'
 
-function home() {
-  return (
-    <div>
-    <div className='main-background'>
-        <h1 className='home-heading'> Esports</h1>
-        <p className='home-paragraph'> Experience the Adrenaline Rush of Esports.</p>
-    </div>
+function Home() {
+  return (<>
+<div className='main-background'>
 
-    <div>
-    <Homecard/>
-    <Homecard/>
+<h1 className='home-heading' style={{color:THEME.highlight}}> {MAIN_TITLE}</h1>
+
+<div className='paragraph-container'>
+  <p className='home-paragraph'> {TAG_LINE}</p>
+  <p className='home-paragraph2'> {TAG_LINE2}</p>
+  <button  type='button'  className='homepage-btn'> Learn more</button>
+ </div>
     
+</div>
 
-    </div>
-    </div>
-  )
+<div className='esport-card-container' style={{backgroundColor:"black"}}>
+    { ESPORTS_TEAM.map((Esports,i)=>{
+
+ return (<Homecard
+   key={i}
+    imgUrl={Esports.imgUrl}
+     title={Esports.title}
+     description={Esports.description}
+    
+ />)
+})
 }
+</div>
 
-export default home
+<div> 
+<iframe ></iframe>
+  </div></>
+)}
+
+export default Home
