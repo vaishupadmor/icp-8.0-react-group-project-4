e39bd5764e95e97afa8d84bc676eac84b19624cf
vaishupadmor <vaishupadmor@gmail.com>
2024-09-10 22:33:59 +0530

    code-added

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -5,20 +5,26 @@ import "./footer.css"
 function Footer() {
   return (
     <div className='footer'>
-        <div className='footer-main-comtainer'>
-        <div>
-         <h4> built for Gamers</h4>
-        <a href=''> <span> Esports</span></a>
-        <a href=''><span>learn esports </span></a>
-        <a href=''><span>Try playing </span></a>
-        <a href=''><span>tournaments </span></a>
+        <div className='footer-main-container'>
+        <div className='footer-main-1'>
+         <h3 className='footer-head'> built for Gamers</h3>
+
+         <span ><a href='' className='footer-links'>Esports</a></span>
+         <span><a href='' className='footer-links'>Try playing </a></span>
+         <span><a href=''  className='footer-links'>tournaments</a> </span>
         
         </div>
-        <div>
-
+        <div className='footer-main-1'>
+          <h3 className='footer-head'> About us</h3>
+          <span> <a> contact us </a></span>
+          <span> <a> follow us</a></span>
+          <span> <a> Get in touch</a></span>
         </div>
-        <div>
-
+        <div className='footer-main-1'>
+          <h3 className='footer-head'> Social</h3>
+          <span> <a> instagram</a></span>
+          <span> <a> facebook</a></span>
+          <span> <a> twitter</a></span>
         </div>
         </div>
         <hr></hr>
