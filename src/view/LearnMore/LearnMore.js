aba5bf161212aaa6f1a22a42cd6e0cd525d073ea
ankitakangane <kanganeankita1@gmail.com>
2024-09-12 17:08:14 +0530

    game description

--- src/view/LearnMore/LearnMore.js
+++ src/view/LearnMore/LearnMore.js
@@ -13,6 +13,10 @@ function LearnMore() {
       </div>
 
       <div  className='card-description-container'>
+      <p className='card-description'>
+        Free Fire is a free-to-play battle royale game developed and published by Garena for Android and iOS. It was released on 8 December 2017. It became the most downloaded mobile game globally in 2019 and has over 1 billion downloads on Google Play Store. In the first quarter of 2021 it was the highest
+        </p>
+
 
       </div>
     </div>
