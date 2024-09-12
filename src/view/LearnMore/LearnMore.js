cd9504cee776fa6c9158f24145e37e7de10d68f6
ankitakangane <kanganeankita1@gmail.com>
2024-09-12 17:14:29 +0530

    css for card-description and card-title

--- src/view/LearnMore/LearnMore.js
+++ src/view/LearnMore/LearnMore.js
@@ -6,20 +6,21 @@ function LearnMore() {
   return (
     <div>
       <div className='card'>
-      <div className='card-img-container'>
-        <img src={ImgGame}
-        alt="free_fire game"
-        className='card-img'
-        />
-      </div>
+        <div className='card-img-container'>
+          <img src={ImgGame}
+            alt="free_fire game"
+            className='card-img'
+          />
+        </div>
 
-      <div  className='card-description-container'>
-      <p className='card-description'>
-        Free Fire is a free-to-play battle royale game developed and published by Garena for Android and iOS. It was released on 8 December 2017. It became the most downloaded mobile game globally in 2019 and has over 1 billion downloads on Google Play Store. In the first quarter of 2021 it was the highest
-        </p>
+        <div className='card-description-container'>
+          <h2 className='card-title'>Free Fire</h2>
+          <p className='card-description'>
+            Free Fire is a free-to-play battle royale game developed and published by Garena for Android and iOS. It was released on 8 December 2017. It became the most downloaded mobile game globally in 2019 and has over 1 billion downloads on Google Play Store. In the first quarter of 2021 it was the highest
+          </p>
 
 
-      </div>
+        </div>
       </div>
     </div>
   )
