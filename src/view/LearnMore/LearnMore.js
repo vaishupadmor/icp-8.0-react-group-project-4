48bc4f9116346d3b4d979217a1fafa5686c84f28
ankitakangane <kanganeankita1@gmail.com>
2024-09-12 16:24:46 +0530

    image added

--- src/view/LearnMore/LearnMore.js
+++ src/view/LearnMore/LearnMore.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import ImgGame from './free_fire.jpg';
 
 function LearnMore() {
   return (
     <div>
       <div className='card-img-container'>
-        
+        <img src={ImgGame}
+        alt="free_fire game"
+        className='card-img'
+        />
       </div>
     </div>
   )
