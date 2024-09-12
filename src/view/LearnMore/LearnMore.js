2d9652fff7b7ed8881b61dd439113e83e1bd10d2
ankitakangane <kanganeankita1@gmail.com>
2024-09-12 16:13:45 +0530

    basic code

---
+++ src/view/LearnMore/LearnMore.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function LearnMore() {
+  return (
+    <div>
+      <div className='card-img-container'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default LearnMore
