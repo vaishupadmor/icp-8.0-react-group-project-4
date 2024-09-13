94f0ec230bd72f70ad7c44fbf8fc63dbda05f0b4
nehapansare <nehapansare007@gmail.com>
2024-09-13 12:14:17 +0530

    tournament page import added

---
+++ src/component/tournaments/tournament.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './tournament.css';
+import trophy from '../../img/trophy.png'; 
+import group from '../../img/group.png';
+import { Link } from 'react-router-dom';
+import { products } from './config/dataa';
+import Navbar from '../tournaments/navbar/navbar'
+
+function tournament() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default tournament
+
