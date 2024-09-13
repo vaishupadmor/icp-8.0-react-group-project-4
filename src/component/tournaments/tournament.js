f558a32b8ce19d16c5b8ca935f89b45112a111be
nehapansare <nehapansare007@gmail.com>
2024-09-13 12:22:55 +0530

    tournament page code added

--- src/component/tournaments/tournament.js
+++ src/component/tournaments/tournament.js
@@ -8,9 +8,39 @@ import Navbar from '../tournaments/navbar/navbar'
 
 function tournament() {
   return (
-    <div>
-      
-    </div>
+    <div className='cart-container'>
+    <Navbar/>
+    {products.map((productItem, index) => (
+      <div className='cart' key={index}>
+        <img src={productItem.imgUrl} alt={productItem.title} className='cart-img' />
+        <hr />
+        <p><b>{productItem.title}</b></p>
+        <hr />
+        <p className='cart-mode'>
+          <span className='cart-text1'><b>{productItem.mode}</b></span>
+          <span className='cart-text2'>
+            <b>{productItem.platform}&nbsp;</b>
+            <i className="fa-solid fa-desktop"></i>
+          </span>
+        </p>
+
+        <p className='cart-mode'>
+          <span className='cart-text1'>
+            <img src={myImg} alt='trophy' className='cart-icon' /> {productItem.prize}
+          </span>
+          <span className='cart-text2'>
+            <img src={myImg1} alt='participants' className='cart-icon' /> {productItem.participants}
+          </span>
+        </p>
+        
+        <p className='cart-detail'>
+          <Link to={productItem.link}>{productItem.status}</Link>
+        </p>
+        <hr />
+        <p><b>{productItem.date}</b></p>
+      </div>
+    ))}
+  </div>
   )
 }
 
