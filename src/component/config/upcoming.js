1a9e85072f6b3aa82f80273a9cf9a2b1be63a50e
nehapansare <nehapansare007@gmail.com>
2024-09-13 18:19:29 +0530

    tournament upcoming config  added

---
+++ src/component/config/upcoming.js
@@ -0,0 +1,74 @@
+
+const products = [
+    {
+      imgUrl: 'https://theesports.club/_next/image?url=https%3A%2F%2Fcdn.theesports.club%2FMini%2520Banner%2520MK1-c0a2c7c3-6cbc-41ea-a296-8ef209717174.jpg&w=1920&q=75',
+      title: "TEC SHOWDOWN SERIES SEASON-4",
+      mode: 'Game Mode: Single Elimination',
+      platform: 'Platform:',
+      prize: "100000000",
+      participants: "8 of 128",
+      status: "UPCOMING",
+      date: "20 Sept 2024",
+      link: "/xyz-tro-123"  // You can add a link path for routing
+    },
+    {
+      imgUrl: 'https://theesports.club/_next/image?url=https%3A%2F%2Fcdn.theesports.club%2FMini-Banner%2520%25282%2529-90cea3a6-4510-4790-be9e-d2b75003833b.png&w=1920&q=75',
+      title: "Intel Gamer Days - Lenovo, Valorant 5v5",
+      mode: 'Game Mode: Single Elimination',
+      platform: 'Platform:',
+      prize: "100000000",
+      participants: "8 of 128",
+      status: "UPCOMING",
+      date: "20 Sept 2024",
+      link: "/xyz-tro-123"  // You can add a link path for routing
+    },
+    {
+      imgUrl: 'https://theesports.club/_next/image?url=https%3A%2F%2Fcdn.theesports.club%2FMini-Banner%2520%25283%2529-d87441dd-2236-4ec3-a3f0-36ee0da366bc.png&w=1920&q=75',
+      title: "MidfailYT Community Clash",
+      mode: 'Game Mode: Single Elimination',
+      platform: 'Platform:',
+      prize: "100000000",
+      participants: "8 of 128",
+      status: "UPCOMING",
+      date: "20 Sept 2024",
+      link: "/xyz-tro-123"  
+    },
+    {
+        imgUrl: 'https://theesports.club/_next/image?url=https%3A%2F%2Fcdn.theesports.club%2FMini-Banner%2520%25283%2529-a2932703-0beb-4dee-916c-6adc06d8eca0.png&w=1920&q=75',
+        title: "Intel Gamer Days - Vishal Peripherals, Telangana (Valorant)",
+        mode: 'Game Mode: Single Elimination',
+        platform: 'Platform:',
+        prize: "100000000",
+        participants: "8 of 128",
+        status: "UPCOMING",
+        date: "20 Sept 2024",
+        link: "/xyz-tro-123"  
+      },
+      {
+        imgUrl: 'https://theesports.club/_next/image?url=https%3A%2F%2Fcdn.theesports.club%2FSMALL%2520banner%2520%25281%2529-ef343f0f-397d-488c-836f-fa3a4938a896.png&w=1920&q=75',
+        title: "Intel Gamer Days - Vishal Peripherals, Telangana (EAFC)",
+        mode: 'Game Mode: Single Elimination',
+        platform: 'Platform:',
+        prize: "100000000",
+        participants: "8 of 128",
+        status: "UPCOMING",
+        date: "20 Sept 2024",
+        link: "/xyz-tro-123"  
+      },
+      {
+        imgUrl: 'https://theesports.club/_next/image?url=https%3A%2F%2Fcdn.theesports.club%2FMini-Banner%2520%25285%2529-5b06a666-6cfd-4f2f-8c05-7a04ca9ef8ea.png&w=1920&q=75',
+        title: "Intel Gamer Days - Vishal Peripherals, Telangana (MK1)",
+        mode: 'Game Mode: Single Elimination',
+        platform: 'Platform:',
+        prize: "100000000",
+        participants: "8 of 128",
+        status: "UPCOMING",
+        date: "20 Sept 2024",
+        link: "/xyz-tro-123"  
+      }
+
+  ];
+
+export{
+    products
+}
