12ddbb37fd5932ab287ce89f3e0d2e3d22e7dfe5
kunalsonwane77 <kunalsonwane77@gmail.com>
2024-09-14 14:26:20 +0530

    created basic structure

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from "./view/home/home";
 import { Toaster } from 'react-hot-toast';
+import About from './view/About/About';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -17,6 +18,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
   {
     path:"*",
     element:<h1> 404 Not found</h1>
+  },
+  {
+    path:"about",
+    element:<About/>
   }
  ])
 root.render( 
---
+++ src/view/About/About.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./About.css"
+
+function About() {
+    return (
+        <div>
+            
+            <div className='aboutcontainer'>
+
+                
+                
+                <div className='aboutcontent'>
+
+                <div className="head">
+                <h1>About us</h1>
+                </div>
+
+                     
+                    <p className='abpara'>Welcome to Esport, your ultimate destination for competitive esports tournaments!
+                         We are passionate about bringing gamers from around the globe together to compete, have fun, and showcase
+                          their skills in some of the worlds most popular and thrilling games.</p>
+
+                          
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default About
