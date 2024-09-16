d05f0b85aff966a3f5ee59ef27a96d2765335cdb
Maheshwarigadakh <maheshwarigadakh2003@gmail.com>
2024-09-16 07:20:07 +0530

    1 text content add

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -3,7 +3,11 @@ import "./contact.css"
 
 function Contact() {
   return (
+    <div>
     <h2>Contact Form</h2>
+    <label>First Name</label>
+    <input type= "text" required placeholder='Enter your first name'></input>
+    </div>
   )
 }
 
