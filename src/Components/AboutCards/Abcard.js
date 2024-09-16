eddcf4d7ac428f0b2800ff68723aed56c2404daf
kunalsonwane77 <kunalsonwane77@gmail.com>
2024-09-16 15:48:58 +0530

    cards added

--- src/Components/AboutCards/Abcard.js
+++ src/Components/AboutCards/Abcard.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import "./Abcard.css"
 
-function Abcard({title,discription}) {
+function Abcard({title,discription,}) {
   return (
     <div>
         <div className="acard">
 
         
         <h1 className='cardtitle'>{title}</h1>
-        <p>{discription}</p>
+        <p className='dic'>{discription}</p>
+       
  
         </div>
     </div>
