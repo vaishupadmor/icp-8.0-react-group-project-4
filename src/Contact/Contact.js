76b7b489f86740ffc4e1c280208fcb913aa99e16
Maheshwarigadakh <maheshwarigadakh2003@gmail.com>
2024-09-16 11:45:46 +0530

    heading style add

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -4,7 +4,7 @@ import "./contact.css"
 function Contact() {
   return (
     <div>
-    <h2>Contact Form</h2>
+    <h2 className='heading'>Contact Form</h2>
     <label>First Name</label>
     <input type= "text" required placeholder='Enter your first name'></input>
     <label>last Name</label>
