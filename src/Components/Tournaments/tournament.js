b76ffa8b8178d3c81d671a15b7e6084789f6866a
nehapansare <nehapansare007@gmail.com>
2024-09-17 17:34:34 +0530

    classname added

--- src/Components/Tournaments/tournament.js
+++ src/Components/Tournaments/tournament.js
@@ -13,7 +13,7 @@ function Tournament() {
     <div className='cart-container'>
       <Navbar/>
     {products.map((productItem, index) => (
-      <div className='cart' key={index}>
+      <div className='cart-tour' key={index}>
         <img src={productItem.imgUrl} alt={productItem.title} className='cart-img' />
         <hr />
         <p><b>{productItem.title}</b></p>
