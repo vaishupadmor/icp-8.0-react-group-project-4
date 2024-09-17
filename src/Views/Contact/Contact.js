cd48ae1b9b353d5b64f82e13d8415c880e8891c7
Maheshwarigadakh <maheshwarigadakh2003@gmail.com>
2024-09-17 12:20:20 +0530

    toaster and style add

--- src/Views/Contact/Contact.js
+++ src/Views/Contact/Contact.js
@@ -1,29 +1,44 @@
 import React from 'react'
 import "./contact.css"
+import toast, { Toaster } from "react-hot-toast"
 
 function Contact() {
   return (
     <div className='contact'>
       <form>
-    <h2 className='heading'>Contact Form</h2>
-    <div className='input-box'>
-    <label>First Name</label>
-    <input type= "text" required placeholder='Enter your first name' className='field'></input>
-    </div>
-    <div className='input-box'>
-    <label>last Name</label>
-    <input type= "text" required placeholder='Enter your last name' className='field'></input>
-    </div>
-    <div className='input-box'>
-    <label>Email</label>
-    <input type= "email" required placeholder='Enter your email address' className='field'></input>
-    </div>
-    <div className='input-box'>
-    <label>Message</label>
-    <input type= "text" required placeholder='Enter message' className='field'></input>
-    </div>
-    <button type="submit">Submit</button>
-    </form>
+        <h2 className='heading'>Contact Form</h2>
+        <div className='input-box'>
+          <label>First Name</label>
+          <input type="text" required placeholder='Enter your first name' className='field'></input>
+        </div>
+        <div className='input-box'>
+          <label>last Name</label>
+          <input type="text" required placeholder='Enter your last name' className='field'></input>
+        </div>
+        <div className='input-box'>
+          <label>Email</label>
+          <input type="email" required placeholder='Enter your email address' className='field'></input>
+        </div>
+        <div className='input-box'>
+          <label>Message</label>
+          <input type="text" required placeholder='Enter message' className='field'></input>
+        </div>
+        <button type="submit" className='btn' onClick={() => {
+          toast.success("Submit Successfully ", {
+            icon: "🚀",
+            duration: 5000,
+            style: {
+              borderRadius: "13px",
+              backgroundColor: 'orange',
+              fontSize: "25px"
+            }
+          })
+        }}>
+          Submit
+        </button>
+      </form>
+
+      <Toaster />
     </div>
   )
 }
