4453a95a62e806974e9a12905ef35850bbe1efef
Maheshwarigadakh <maheshwarigadakh2003@gmail.com>
2024-09-17 12:03:34 +0530

    input box style add

--- src/Views/Contact/Contact.js
+++ src/Views/Contact/Contact.js
@@ -6,14 +6,22 @@ function Contact() {
     <div className='contact'>
       <form>
     <h2 className='heading'>Contact Form</h2>
+    <div className='input-box'>
     <label>First Name</label>
     <input type= "text" required placeholder='Enter your first name'></input>
+    </div>
+    <div className='input-box'>
     <label>last Name</label>
     <input type= "text" required placeholder='Enter your last name'></input>
+    </div>
+    <div className='input-box'>
     <label>Email</label>
     <input type= "email" required placeholder='Enter your email address'></input>
+    </div>
+    <div className='input-box'>
     <label>Message</label>
     <input type= "text" required placeholder='Enter message'></input>
+    </div>
     <button type="submit">Submit</button>
     </form>
     </div>
