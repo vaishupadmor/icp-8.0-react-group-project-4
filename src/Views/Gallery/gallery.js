c22302f813b08c9f6d4fc612cf25a5f340a70b30
khardeaditya <khardeaditya240@gmail.com>
2024-09-17 10:38:21 -0700

    navbar

--- src/Views/Gallery/gallery.js
+++ src/Views/Gallery/gallery.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./gallery.css"
+import Navbar from '../../Components/Navbar/Navbar';
 import { Link } from 'react-router-dom';
 import { TEAMS } from './../../Config/gallerydata';
 
@@ -8,8 +9,10 @@ import { TEAMS } from './../../Config/gallerydata';
 function Tournament() {
   return (
     <div className='cart-container'>
+           <Navbar/>
     {TEAMS.map((team, index) => (
       <div className='cart' key={index}>
+     
         <img src={team.imgUrl} alt={team.title} className='cart-img' />
        
         <h2 className='teamname'>{team.name}</h2>
