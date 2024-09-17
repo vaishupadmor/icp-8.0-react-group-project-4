90e5b1aa842e95d78cd620829f03972b62e44008
Maheshwarigadakh <maheshwarigadakh2003@gmail.com>
2024-09-17 14:21:43 +0530

    address link

--- src/Views/Contact/Contact.js
+++ src/Views/Contact/Contact.js
@@ -46,21 +46,17 @@ function Contact() {
           <div className='container'>
           <div className='main-info'>Location
             <br/>
-            <p>Registered office:
-G-36, 1st Floor, G-Block,
-Outer Circle, Connaught Place,
-New Delhi
-PinCode: 110001</p>
+            <p><a href="https://www.google.com/maps/place/%E0%A4%95%E0%A5%8B%E0%A4%A8%E0%A5%89%E0%A4%9F+%E0%A4%AA%E0%A5%8D%E0%A4%B2%E0%A5%87%E0%A4%B8,+%E0%A4%A8%E0%A4%B5%E0%A5%80+%E0%A4%A6%E0%A4%BF%E0%A4%B2%E0%A5%8D%E0%A4%B2%E0%A5%80,+%E0%A4%A6%E0%A4%BF%E0%A4%B2%E0%A5%8D%E0%A4%B2%E0%A5%80+110001/@28.6289016,77.2049872,15z/data=!3m1!4b1!4m6!3m5!1s0x390cfd37b741d057:0xcdee88e47393c3f1!8m2!3d28.6304203!4d77.2177216!16zL20vMDR4eDB4?entry=ttu&g_ep=EgoyMDI0MDkxMS4wIKXMDSoASAFQAw%3D%3D"> Connaught Place,New Delhi</a></p>
             <img src={location} className='address'></img>
           </div>
          
           <div className='main-info'>Email
             <br/>
-            <p>esports@gmail.com</p>
+            <p ><a href="mailto:%20contact@esportsgroup.net">esports@gmail.com</a></p>
             <img src={email} className='address'></img>
           </div>
           <div className='main-info'>Contact<br/>
-          <p>+88695864</p>
+          <p><a href="tel:8869586487">+8869586487</a></p>
         <img src={mobile} className='address call'></img>
           </div>
           </div>
