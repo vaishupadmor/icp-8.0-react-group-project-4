11c6446d335fed59d5ce0709751574fd624f874f
vaishupadmor <vaishupadmor@gmail.com>
2024-09-19 11:48:16 +0530

    -

--- src/Components/ImgSlider/ImgSlider1.js
+++ src/Components/ImgSlider/ImgSlider1.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import lessthan from "./less-than.png"
 import greaterthan from "./greater-than.png"
-import './ImgSlider.css'; 
+import './ImgSlider1.css'; 
 
 const ImageSlider = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
