28760e7f8fecb4a1acf8425a3241c71dc25c5902
vaishupadmor <vaishupadmor@gmail.com>
2024-09-19 11:36:22 +0530

    code-added

---
+++ src/Config/FollowUs.js
@@ -0,0 +1,19 @@
+const FollowUs =[
+    {
+        id:"1",
+        pageUrl:"",
+    },
+    {
+        id:"2",
+        pageUrl:"",
+    },
+    {
+        id:"3",
+        pageUrl:"",
+    },
+    {
+        id:"4",
+        pageUrl:"",
+    },
+
+]
