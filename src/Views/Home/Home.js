8754f2e26de4f37f836dd9265fd4c2270cfd220a
vaishupadmor <vaishupadmor@gmail.com>
2024-09-19 16:24:29 +0530

    code-added

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -12,7 +12,7 @@ import style from './style.module.css'
 import { useSpring, a ,animated} from '@react-spring/web'
 import Navbar from "./../../Components/Navbar/Navbar"
 import Logo from "./logo.png"
-import { FollowUs } from '../../Config/FollowUs'
+
 
 
 
