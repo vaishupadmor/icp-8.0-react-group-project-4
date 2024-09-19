0010ccf5a894edf4d70002efc352ced0f0c7d83a
vaishupadmor <vaishupadmor@gmail.com>
2024-09-19 16:23:27 +0530

    code-added

--- src/Components/HomeCards/HomeCard1.js
+++ src/Components/HomeCards/HomeCard1.js
@@ -1,26 +1,32 @@
-import React from 'react'
-import "./HomeCard1.css"
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import "./HomeCard1.css";
 
-function Homecard({imgUrl,title,description,FollowUs}) {
-  return (
-    
-       <div className="esport-card">
-        <img
-        src={imgUrl}
-        className='esport-card-img'
-        alt='teams'/>
-       
-        <div className='esport-card-description'>
-        <h1 className='esport-card-name'> {title}</h1>
-        <p > {description}</p>
-        <button className='esport-card-button'> {FollowUs}</button>
+function HomeCard({ imgUrl, title, description, followLinks }) {
+    const [currentLinkIndex, setCurrentLinkIndex] = useState(0);
+
+    const handleFollowUsClick = () => {
+        const currentLink = followLinks[currentLinkIndex];
+        window.open(currentLink, '_blank'); // Opens the current link in a new tab
+        setCurrentLinkIndex((currentLinkIndex + 1) % followLinks.length); // Cycle to the next link
+    };
+
+    return (
+        <div className="esport-card">
+            <img
+                src={imgUrl}
+                className='esport-card-img'
+                alt='teams'
+            />
+            <div className='esport-card-description'>
+                <h1 className='esport-card-name'> {title}</h1>
+                <p> {description}</p>
+                
+            </div>
+            <button className='esport-card-button' onClick={handleFollowUsClick}>
+                    Follow Us
+                </button>
         </div>
-        
-    </div>
-    
-  )
+    );
 }
 
-
-export default Homecard
+export default HomeCard;
