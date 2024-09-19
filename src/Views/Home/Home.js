33af9fe4c58fbaab31c05b377113627d0897714e
vaishupadmor <vaishupadmor@gmail.com>
2024-09-19 11:46:16 +0530

    code-added

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -1,12 +1,12 @@
 import React, { useState,useEffect } from 'react'
 import "./Home.css"
-import Homecard from '../../Components/HomeCards/Homecard'
+import Homecard from '../../Components/HomeCards/HomeCard'
 import { ESPORTS_TEAM ,TAG_LINE,THEME,MAIN_TITLE,TAG_LINE2, } from '../../Config/Data'
 import Footer from "../../Components/Footer/Footer"
-import TopTournament from '../../Components/TopTournament/TopTournament'
+import TopTournament from '../../Components/TopTournament/TopTournament1'
 import { TOURNAMENTS } from '../../Config/Data1'
 import { Link } from 'react-router-dom'
-import ImageSlider from '../../Components/ImgSlider/ImgSlider'
+import ImageSlider from '../../Components/ImgSlider/ImgSlider1'
 import VideoPlayer from '../../Components/YoutubeVideo/VideoPlayer'
 import style from './style.module.css'
 import { useSpring, a } from '@react-spring/web'
