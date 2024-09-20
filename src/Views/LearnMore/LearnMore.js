752c4436972c1fbe190803b06ed2d0a8da99ede9
ankitakangane <kanganeankita1@gmail.com>
2024-09-20 21:08:29 +0530

    game info

--- src/Views/LearnMore/LearnMore.js
+++ src/Views/LearnMore/LearnMore.js
@@ -37,7 +37,7 @@ function LearnMore() {
       <div className='card'>
         <div className='card-img-container'>
           <img src={ImgGame}
-            alt="free_fire game"
+            alt="game"
             className='card-img'
           />
         </div>
@@ -46,10 +46,10 @@ function LearnMore() {
           <h2 className='card-title'>Free Fire</h2>
           
           <p className='card-description'>
-          Free Fire is a free-to-play battle royale game developed and published by Garena for Android and iOS. It was released on 8 December 2017...
+          Free Fire is a free-to-play battle royale game developed and published by Garena for Android and iOS. It was released on 8 December 2017 .....
           </p>
           <p className='card-description'>
-          Free Fire has emerged as one of the premiere go-to battle royale games on the mobile market, beating Call of Duty: Mobile and running close against PUBG Mobile. In your quest to be the last one standing each game, you must know how the action game functions! The game is quite simple for anyone to pick up, but it only takes a special one to master the mechanics and rise to the top of the rankings! If you’re among the latter, then this beginner’s guide will be especially handy!!!  
+          Free Fire has emerged as one of the premiere go-to battle royale games on the mobile market, beating Call of Duty: Mobile and running close against PUBG Mobile. In your quest to be the last one standing each game, you must know how the action game functions! The game is quite simple for anyone to pick up, but it only takes a special one to master the mechanics and rise to the top of the rankings! If you’re among the latter, then this beginner’s guide will be especially handy.Free Fire is another very popular battle royale game for the mobile that has been developed by Garena. !!!!!  
           </p>
 
         </div>
@@ -58,7 +58,7 @@ function LearnMore() {
       <div className='card'>
         <div className='card-img-container'>
           <img src={ImgBadlandersGame}
-            alt="free_fire game"
+            alt="game"
             className='card-img'
           />
         </div>
@@ -67,10 +67,10 @@ function LearnMore() {
           <h2 className='card-title'>Badlanders</h2>
 
           <p className='card-description'>
-          Badlanders Developer: Netease Release Date: 2022-01-14...
+          Badlanders Developer: Netease Release Date: 2022-01-14 .....
           </p>
           <p className='card-description'>
-            Simple! However, in contrast to games like PUBG Mobile or Free Fire, the objective in Badlanders is not necessarily to kill everyone as there are other ways to win a match. Instead of fighting, you can choose to play stealthily, and simply escape from the map via the extraction points that open up during the match!!!
+            Simple! However, in contrast to games like PUBG Mobile or Free Fire, the objective in Badlanders is not necessarily to kill everyone as there are other ways to win a match. Instead of fighting, you can choose to play stealthily, and simply escape from the map via the extraction points that open up during the match !!!!!
           </p>
         </div>
       </div>
@@ -78,7 +78,7 @@ function LearnMore() {
       <div className='card'>
         <div className='card-img-container'>
           <img src={ImgPubgGame}
-            alt="free_fire game"
+            alt="game"
             className='card-img'
           />
         </div>
@@ -86,11 +86,11 @@ function LearnMore() {
         <div className='card-description-container'>
           <h2 className='card-title'>Pubg</h2>
           <p className='card-description'>
-          A version for the Stadia streaming platform was released in April 2020, with Xbox Series X/S and PlayStation 5 versions being released in November 2020. The game has been free-to-play for all platforms since January 12, 2022... 
+          A version for the Stadia streaming platform was released in April 2020, with Xbox Series X/S and PlayStation 5 versions being released in November 2020. The game has been free-to-play for all platforms since January 12, 2022 ..... 
           </p>
 
           <p className='card-description'>
-           Keep an eye on the shrinking play zone (safe zone) and move accordingly to avoid taking damage from the blue zone. Use vehicles to move across the map quickly, but be cautious as they can attract attention. It is best always to be aware of your surroundings. Remember to use objects on the map for cover!!!
+           Keep an eye on the shrinking play zone (safe zone) and move accordingly to avoid taking damage from the blue zone. Use vehicles to move across the map quickly, but be cautious as they can attract attention. It is best always to be aware of your surroundings. Remember to use objects on the map for cover !!!!!
           </p>
         </div>
       </div>
@@ -98,7 +98,7 @@ function LearnMore() {
       <div className='card'>
         <div className='card-img-container'>
           <img src={ImageKabaddi}
-            alt="free_fire game"
+            alt="game"
             className='card-img'
           />
         </div>
@@ -107,7 +107,7 @@ function LearnMore() {
           <h2 className='card-title'>Cricket</h2>
           
           <p className='card-description'>
-            Recognize the goal of the game. As in most field games, the goal of cricket is to score points, called runs, against the opposing team by running from one point to another before the play can be ended or you're run out by the defenders, who are called the “fielding team.” The team at bat is called the “batting team
+            Recognize the goal of the game. As in most field games, the goal of cricket is to score points, called runs, against the opposing team by running from one point to another before the play can be ended or you're run out by the defenders, who are called the “fielding team.” The team at bat is called the “batting team .....
           </p>
         </div>
       </div>
