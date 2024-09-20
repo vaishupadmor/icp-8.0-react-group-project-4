160c6de556eb4f198acf8d883ad8d803656e84c0
Harshofft <hdharsh122@gmail.com>
2024-09-20 20:28:57 +0530

    shop work completed

--- src/Views/Gocart/gocart.js
+++ src/Views/Gocart/gocart.js
@@ -2,25 +2,67 @@ import React, { useState, useEffect } from 'react';
 import './gocart.css';
 import { useParams } from 'react-router-dom';
 import { Shopcard } from './../../Config/shopcard';
-
+import Imgpng from './../Shop/shopimgs/img5.png'
+import Imggod from './../Shop/shopimgs/wp10278791-god-of-war-symbol-wallpapers-removebg-preview.png'
+import { to } from 'react-spring';
 
 const App = () => {
     const { cartID } = useParams();
     const [data, setData] = useState({
         name: '',
         price: '',
+        totalprice: '',
         imgurl: '',
         description: ''
     });
     useEffect(() => {
         const Datacart = Shopcard.find((item) => { return item.id == cartID })
         setData(Datacart);  
+        
         localStorage.setItem('data', JSON.stringify(Datacart));
+        // if (cartID ==1) {
+        //     const data1 = Shopcard.find((item) => { return item.id == 1 })
+        //     setData(data1);
+            
+        // localStorage.setItem('data1', JSON.stringify(data1));
+        // }
+        // else if (cartID ==2) {
+        //     const data2 = Shopcard.find((item) => { return item.id == 2 })
+        //     setData(data2);
+            
+        // localStorage.setItem('data2', JSON.stringify(data2));
+        // }
+        // else if (cartID ==3) {
+        //     const data3 = Shopcard.find((item) => { return item.id == 3 })
+        //     setData(data3);
+            
+        // localStorage.setItem('data3', JSON.stringify(data3));
+        // }
+        // else if (cartID ==4) {
+        //     const data4 = Shopcard.find((item) => { return item.id == 4 })
+        //     setData(data4);
+            
+        // localStorage.setItem('data4', JSON.stringify(data4));
+        // }
+        // else if (cartID ==5) {
+        //     const data5 = Shopcard.find((item) => { return item.id == 5 })
+        //     setData(data5);
+            
+        // localStorage.setItem('data5', JSON.stringify(data5));
+        // }
+        // else if (cartID ==6) {
+        //     const data6 = Shopcard.find((item) => { return item.id == 6 })
+        //     setData(data6);
+            
+        // localStorage.setItem('data6', JSON.stringify(data6));
+        // }
+        
         
     }, [cartID]);
 
-
+    
     return (
+        <div className="container-game">
         <div className="main-container">
 
             <div className="detail-game">
@@ -42,6 +84,7 @@ const App = () => {
                                 imgurl: '',
                                 description: ''
                             })
+                            localStorage.removeItem('data');
                         }}  >Remove From Cart
                             <span>
                                 <svg className="" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
@@ -52,6 +95,21 @@ const App = () => {
                     </div>
                 </div>
             </div>
+        </div>
+        <div className='totalprice-con'>
+            <div className='totalprice-con-1'>
+                <p className='amount'>Price : </p>
+                <p className='amount'>Tax : 30%</p>
+                <br/>
+                <p className='amount'>Totalprice - discount(500) : </p>
+            </div>
+            <div className='totalprice-con-2'>
+                <p >{data.price}</p>
+                <p>₹{data.totalprice}</p>
+                <br/>
+                <p className='pricetag amount'>₹{data.totalprice-500}</p>
+            </div>
+        </div>
         </div>
     )
 }
