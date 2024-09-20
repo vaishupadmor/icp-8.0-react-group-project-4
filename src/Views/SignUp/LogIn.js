2e00a899f6527570ea3c6f4ca86a08a864eceead
nehapansare <nehapansare007@gmail.com>
2024-09-20 23:48:02 +0530

    classname changed

--- src/Views/SignUp/LogIn.js
+++ src/Views/SignUp/LogIn.js
@@ -36,8 +36,8 @@ function Login() {
   return (
     <div className="container">
       <div className="form-container">
-        <form className="form" onSubmit={handleSubmit}>
-          <div className="title">Login</div>
+        <form className="form1" onSubmit={handleSubmit}>
+          <div className="title-signup">Login</div>
           <input type="text" name="username" placeholder="Username (Email)" required className="input-field" />
           <input type="password" name="password" placeholder="Password" required className="input-field" />
           <button type="submit" className="button">Login</button>
